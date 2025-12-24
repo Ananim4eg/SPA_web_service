8f87a3dd910354fdac0bd3a9df52e9f89bf1c51d
Ananim4eg <109145711+Ananim4eg@users.noreply.github.com>
2025-12-24 09:59:47 +0300

    create app users

--- config/settings.py
+++ config/settings.py
@@ -1,8 +1,9 @@
 import os
 from pathlib import Path
 
-from django.conf.global_settings import MEDIA_URL
 from dotenv import load_dotenv
+
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,6 +24,7 @@ INSTALLED_APPS = [
 
     "rest_framework",
     "materials",
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -100,3 +102,5 @@ STATIC_URL = "static/"
 
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = 'users.CustomUser'
