c118c65401cc6a0cedbddf8e3ff6102b9d66f2c5
Ananim4eg <109145711+Ananim4eg@users.noreply.github.com>
2025-12-29 00:42:22 +0300

    release generics

--- materials/urls.py
+++ materials/urls.py
@@ -1,7 +1,9 @@
+from django.urls import path
 from materials.apps import MaterialsConfig
 from rest_framework.routers import DefaultRouter
 
-from materials.views import CourseViewSet
+from materials.views import CourseViewSet, LessonCreateApiView, LessonListApiView, LessonRetrieveApiView, \
+    LessonUpdateApiView, LessonDeleteApiView
 
 app_name = MaterialsConfig.name
 
@@ -9,5 +11,9 @@ router = DefaultRouter()
 router.register(r'courses', CourseViewSet, basename='course')
 
 urlpatterns = [
-
+    path('lesson/create/', LessonCreateApiView.as_view(), name='lesson_create'),
+    path('lesson/', LessonListApiView.as_view(), name='lesson_list'),
+    path('lesson/<int:pk>/', LessonRetrieveApiView.as_view(), name='lesson_get'),
+    path('lesson/update/<int:pk>/', LessonUpdateApiView.as_view(), name='lesson_update'),
+    path('lesson/delete/<int:pk>/', LessonDeleteApiView.as_view(), name='lesson_delete'),
 ] + router.urls
