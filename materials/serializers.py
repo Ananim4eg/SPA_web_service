2fe2943f3568db87fb83479b775d2f5ebcb1be74
Ananim4eg <109145711+Ananim4eg@users.noreply.github.com>
2025-12-30 16:44:49 +0300

    release ordering payment

--- materials/serializers.py
+++ materials/serializers.py
@@ -3,19 +3,20 @@ from rest_framework import serializers
 from materials.models import Course, Lesson
 
 
+class LessonSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Lesson
+        fields = '__all__'
+
+
 class CourseSerializer(serializers.ModelSerializer):
     lessons_count = serializers.SerializerMethodField()
+    lessons_info = LessonSerializer(source='get_lesson',many=True, read_only=True)
 
     def get_lessons_count(self, obj):
         return obj.get_lesson.count()
 
     class Meta:
         model = Course
-        fields = ['id', 'course_name', 'preview_course', 'description', 'lessons_count']
-
-
-class LessonSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Lesson
-        fields = '__all__'
+        fields = ['id', 'course_name', 'preview_course', 'description', 'lessons_count', 'lessons_info']
--- users/views.py
+++ users/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.filters import OrderingFilter
 
-# Create your views here.
+from users.models import Payment
+from users.serializers import PaymentSerializer
+
+
+class PaymentViewSet(viewsets.ModelViewSet):
+    serializer_class = PaymentSerializer
+    queryset = Payment.objects.all()
+    filter_backends = [OrderingFilter,]
+    ordering_fields = ['date', 'course', 'lesson', 'payment_method']
