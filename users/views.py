e6693caabec1d990fe514115d6ca331decdc6513
Ananim4eg <109145711+Ananim4eg@users.noreply.github.com>
2026-01-08 03:07:26 +0300

    release IsAuthenticated for CRUD

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets, status
 from rest_framework.filters import OrderingFilter
 from rest_framework.generics import ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -38,20 +39,24 @@ class UserListView(ListAPIView):
     """Представление для просмотра списка пользователей"""
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class UserDetailView(RetrieveAPIView):
     """Представление для просмотра отдельного пользователя"""
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class UserUpdateView(UpdateAPIView):
     """Представление для обновления пользователя"""
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class UserDeleteView(DestroyAPIView):
     """Представление для удаления пользователя"""
     queryset = CustomUser.objects.all()
+    permission_classes = [IsAuthenticated]
