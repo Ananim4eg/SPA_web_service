545899e6e6f704a733d58df843af42770bf30c18
Ananim4eg <109145711+Ananim4eg@users.noreply.github.com>
2026-01-17 18:34:54 +0300

    add drf-yasg

--- users/views.py
+++ users/views.py
@@ -1,3 +1,5 @@
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import viewsets, status
 from rest_framework.filters import OrderingFilter
 from rest_framework.generics import ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
@@ -27,6 +29,26 @@ class MyTokenObtainPairView(TokenObtainPairView):
 class RegisterView(APIView):
     """Представление для регистрации"""
 
+    @swagger_auto_schema(
+        operation_description="Регистрация",
+        operation_summary="Регистрация нового пользователя",
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                "email": openapi.Schema(type=openapi.TYPE_STRING, description='Почта'),
+                "password": openapi.Schema(type=openapi.TYPE_STRING, description='Пароль'),
+                "confirm_password": openapi.Schema(type=openapi.TYPE_STRING, description='Подтверждение пароля')
+            },
+            required=['email', "password", "confirm_password"]
+        ),
+        responses={
+            201: openapi.Response(
+                description="1"
+            ),
+            400: "Ошибка валидации данных"
+        },
+        tags=['register']
+    )
     def post(self, request):
         serializer = UserRegistrationSerializer(data=request.data)
         if serializer.is_valid():
